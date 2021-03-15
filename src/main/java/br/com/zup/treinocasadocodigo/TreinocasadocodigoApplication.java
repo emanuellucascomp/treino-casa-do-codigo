a3f553ec6caf4f2ef01b4f49aab78922a0e4f071
emanuellucascomp <cardgameheritage@gmail.com>
2021-03-15 09:36:21 -0300

    bootstrap do projeto

---
+++ src/main/java/br/com/zup/treinocasadocodigo/TreinocasadocodigoApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.treinocasadocodigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreinocasadocodigoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreinocasadocodigoApplication.class, args);
+	}
+
+}
